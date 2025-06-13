e75cbc4dc9165f207789fc5803d3f441377a54fe
wendemcosta <wsilvadacosta639@gmail.com>
2025-06-13 11:46:52 -0400

    switch_case

---
+++ operadores_logicos/type.js
@@ -0,0 +1,27 @@
+var dia = "quarta";
+
+switch (dia) {
+    case "segunda":
+        console.log("dia de começar a semana");
+    break;
+
+    case "terça":
+        console.log("hora de aprender coisas novas ");
+    break;
+
+    case "quarta":
+        console.log("metade  da semana. continue firme");
+    break;
+
+    case "quinta":
+        console.log("quase lá ! um pouco mais de esforço");
+    break;
+
+    case "sexta":
+        console.log("dia de comemorar, o fim de semana chegou!");
+    break;
+
+    default:
+        console.log("fim de semana")
+ 
+}
