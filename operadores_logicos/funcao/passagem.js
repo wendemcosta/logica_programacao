623e7007519085058fb5a0d518718e445b846692
wendemcosta <wsilvadacosta639@gmail.com>
2025-06-16 11:23:48 -0400

    saudacao.

--- operadores_logicos/funcao/passagem.js
+++ operadores_logicos/funcao/passagem.js
@@ -1,8 +1,14 @@
-function dobrar(num) {
+/*function dobrar(num) {
     valor = num * 2;
     return num;
 }
 var valor = 10;
 console.log(dobrar(valor)); // 20 
-console.log(valor);//10
+console.log(valor);//10*/
+
+function saudacao(nome = "visitante") {
+   console.log(`olá, ${nome}!`);
+} 
+saudacao();//olá,visitante!
+saudacao("wendem"); //olá, wendem!
 
